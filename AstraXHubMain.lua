4ec63855da1572a18b24adb371f4cff9a110c28c
_WT <thieengw9990@gmail.com>
2026-01-24 14:28:27 +0700

    Update AstraXHubMain.lua

--- AstraXHubMain.lua
+++ AstraXHubMain.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/93324f6a94c3cd0c"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/5c416c28167ec6ee9051edf5c83bd362bf5a7c4d1b3e63fe37fd4a1620a06ede/download"))()
